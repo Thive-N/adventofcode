3d3e4ad682677e9d1b0e901d45cf7dc1c6100f37
Thivejan Neranjan <35178093+Thive-N@users.noreply.github.com>
2023-06-12 19:34:12 +0100

    day 15 rust solution update
    few optimizations

--- misc/main.rs
+++ misc/main.rs
@@ -1,45 +1,106 @@
-use std::fs;
 use std::time::Instant;
 
+pub fn get_device_coordinates(input: &str) -> (i64, i64) {
+    let device_split = input.split(" at ").last().unwrap();
+    let x = device_split
+        .split(", ")
+        .next()
+        .unwrap()
+        .split('=')
+        .last()
+        .unwrap()
+        .parse::<i64>()
+        .unwrap();
+    let y = device_split
+        .split(", ")
+        .last()
+        .unwrap()
+        .split('=')
+        .last()
+        .unwrap()
+        .parse::<i64>()
+        .unwrap();
+
+    (x, y)
+}
 fn main() {
+    let start = Instant::now();
     let now = Instant::now();
 
-    let input = fs::read_to_string("2022-15").unwrap();
+    let mut sensors = Vec::with_capacity(40);
+    let mut beacons = Vec::with_capacity(40);
 
-    let mut sensors = Vec::new();
-    let mut beacons = Vec::new();
+    let input = include_str!("../2022-15");
+    input
+        .lines()
+        .filter(|line| !line.is_empty())
+        .for_each(|line| {
+            let mut positions = line.split(": ");
+            let sensor_split = positions.next().unwrap();
+            let beacon_split = positions.next().unwrap();
+            let (sensor_x, sensor_y) = get_device_coordinates(sensor_split);
+            let (beacon_x, beacon_y) = get_device_coordinates(beacon_split);
+            sensors.push((sensor_x, sensor_y));
+            beacons.push((beacon_x, beacon_y));
+        });
 
-    for line in input.lines() {
-        let parts: Vec<_> = line.split(' ').collect();
-        let lx = parts[2].split('=').last().unwrap();
-        let lx = lx[0..lx.len() - 1].parse::<i64>().unwrap();
-
-        let ly = parts[3].split('=').last().unwrap();
-        let ly = ly[0..ly.len() - 1].parse::<i64>().unwrap();
-
-        let bx = parts[8].split('=').last().unwrap();
-        let bx = bx[0..bx.len() - 1].parse::<i64>().unwrap();
-
-        let by = parts[9].split('=').last().unwrap().parse::<i64>().unwrap();
-
-        sensors.push((lx, ly));
-        beacons.push((bx, by));
-    }
-
+    let elapsed = now.elapsed();
+    println!("total time Parsing: {:.2?}", elapsed);
+    let now = Instant::now();
     let N = sensors.len();
+    const Y: i64 = 2000000;
+
+    let mut allowed = Vec::new();
+    let mut intervals = Vec::new();
     let mut dists = Vec::new();
     let mut pos_lines = Vec::new();
     let mut neg_lines = Vec::new();
 
     for i in 0..N - 1 {
         dists.push(dist(sensors[i], beacons[i]));
+        let dx = dists[i] - (sensors[i].1 - Y).abs();
+        if dx > 0 {
+            intervals.push((sensors[i].0 - dx, sensors[i].0 + dx));
+        }
         neg_lines.push(sensors[i].0 + sensors[i].1 - dists[i]);
         neg_lines.push(sensors[i].0 + sensors[i].1 + dists[i]);
 
         pos_lines.push(sensors[i].0 - sensors[i].1 - dists[i]);
         pos_lines.push(sensors[i].0 - sensors[i].1 + dists[i]);
-    }
 
+        if beacons[i].0 == Y {
+            allowed.push(beacons[i].0);
+        }
+    }
+    let elapsed = now.elapsed();
+    println!("total time normalising input: {:.2?}", elapsed);
+    let now = Instant::now();
+    // part 1
+    let mut minx: i64 = i64::MAX;
+    let mut maxx: i64 = i64::MIN;
+    let mut answer: i64 = 0;
+    let l = intervals.len();
+    for i in 1..l {
+        minx = minx.min(intervals[i].0);
+        maxx = maxx.max(intervals[i].1);
+    }
+    for x in minx..maxx + 1 {
+        {
+            if allowed.contains(&x) {
+                continue;
+            }
+            for i in 1..l {
+                if intervals[i].0 <= x || x <= intervals[i].1 {
+                    answer += 1;
+                    break;
+                }
+            }
+        }
+    }
+    let elapsed = now.elapsed();
+    println!("Runtime part1: {:.2?}", elapsed);
+    let now = Instant::now();
+    // part 2
     let mut pos: i64 = i64::MAX;
     let mut neg: i64 = i64::MAX;
     let mut a: i64;
@@ -64,8 +125,11 @@ fn main() {
     }
     let ans = ((pos + neg) / 2) * 4000000 + ((neg - pos) / 2);
     let elapsed = now.elapsed();
-    println!("total time Elapsed: {:.2?}", elapsed);
-    println!("{}", ans);
+    let fullelapse = start.elapsed();
+    println!("Runtime part2: {:.2?}", elapsed);
+    println!("Full time spent {:.2?}", fullelapse);
+    println!("part 1: {}", answer);
+    println!("part 2: {}", ans);
 }
 
 fn dist(a: (i64, i64), b: (i64, i64)) -> i64 {
